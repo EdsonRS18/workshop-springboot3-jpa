0862ddf018617e4ff4cca079919c737ca3317542
Edson Gomes <edson.bol.santos@hotmail.com>
2023-05-31 20:29:24 -0300

    mudando nome do projeto, [200~H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,20 +1,19 @@
-package com.projetoSpring.course.Resources;
-//um recurso web que e implementado por um controlador rest
+package com.educandoweb.course.resources;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.projetoSpring.course.entities.User;
-
+import com.educandoweb.course.entities.User;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1, "maria", "maria@hotmail.com", "888888", "23145");
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
 		return ResponseEntity.ok().body(u);
 	}
 }
