aceefdffcde84355fce32b7a0a0a50e724c0a672
Edson Gomes <edson.bol.santos@hotmail.com>
2023-05-31 15:31:02 -0300

    User entity e resource

---
+++ src/main/java/com/projetoSpring/course/Resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.projetoSpring.course.Resources;
+//um recurso web que e implementado por um controlador rest
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetoSpring.course.entities.User;
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1, "maria", "maria@hotmail.com", "888888", "23145");
+		return ResponseEntity.ok().body(u);
+	}
+}
